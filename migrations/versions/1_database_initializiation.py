97cba9ed4a32b8aae47f73ea8e066d3df85cbce0
Warden Cairn <warden@cairnrpg.com>
2024-09-04 21:12:35 -0400

    initial database

---
+++ migrations/versions/1_database_initializiation.py
@@ -0,0 +1,24 @@
+"""database initializiation
+
+Revision ID: 1
+Revises: 44d2f4ff1d8c
+Create Date: 2024-09-04 21:05:29.016994
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1'
+down_revision = '44d2f4ff1d8c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
