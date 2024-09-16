3f6d756ddcfa11558f8fa29c62b06eb24db82805
David Stearns <stearns.de@gmail.com>
2024-09-16 12:44:32 -0400

    update client socket connection js

--- app/static/src/js/socket_notifications.js
+++ app/static/src/js/socket_notifications.js
@@ -4,20 +4,50 @@ const socketNotificationManager = {
   socket: null,
   notifications: [],
   maxNotifications: 5,
+  connectionAttempts: 0,
+  maxConnectionAttempts: 5,
 
   init() {
-    this.socket = io();
+    this.connectSocket();
+  },
+
+  connectSocket() {
+    if (this.connectionAttempts >= this.maxConnectionAttempts) {
+      console.error("Max connection attempts reached. Please try again later.");
+      return;
+    }
+
+    this.socket = io({
+      transports: ["websocket"],
+      upgrade: false,
+      forceNew: true,
+      reconnection: true,
+      reconnectionAttempts: 3,
+      timeout: 10000,
+    });
+
     this.initializeSocketListeners();
+    this.connectionAttempts++;
   },
 
   initializeSocketListeners() {
     this.socket.on("connect", () => {
       console.log("Connected to Flask server");
+      this.connectionAttempts = 0;
       this.registerUser();
     });
 
-    this.socket.on("disconnect", () => {
-      console.log("Disconnected from Flask server");
+    this.socket.on("connect_error", (error) => {
+      console.error("Connection error:", error);
+      setTimeout(() => this.connectSocket(), 5000);
+    });
+
+    this.socket.on("disconnect", (reason) => {
+      console.log("Disconnected from Flask server:", reason);
+      if (reason === "io server disconnect") {
+        // the disconnection was initiated by the server, reconnect manually
+        setTimeout(() => this.connectSocket(), 5000);
+      }
     });
 
     this.socket.on("dice_rolled", (data) => {
@@ -36,8 +66,13 @@ const socketNotificationManager = {
       party_id: partyId,
       character_id: characterId,
     };
-    this.socket.emit("roll_dice", data);
-    // console.log("Dice roll sent:", data);
+    if (this.socket && this.socket.connected) {
+      this.socket.emit("roll_dice", data);
+      // console.log("Dice roll sent:", data);
+    } else {
+      console.error("Socket not connected. Unable to send dice roll.");
+      notification.showNotification("Unable to send dice roll. Please check your connection.");
+    }
   },
 };
 
