e7ec22b02c9dced8ef9e38261f94155d6e611988
Yochai Gal <yochaigal@gmail.com>
2024-09-16 13:11:06 -0400

    added timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 flask db upgrade
 
 # Start the application
-exec gunicorn -k eventlet -w 2 -b 0.0.0.0:8000 app:application
+exec gunicorn -k eventlet -w 2 -b 0.0.0.0:8000 --timeout 120 app:application
