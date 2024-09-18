b830df79e03e3b887f993d4d72e255eb13c49ed4
Roman Garanin <git@romangaranin.net>
2024-09-18 21:07:16 +0200

    #9 use util function for roll

--- app/static/src/js/tools.js
+++ app/static/src/js/tools.js
@@ -1,3 +1,5 @@
+import utils from "./utils.js";
+
 const categorySelect = document.getElementById("category-select");
 const subcategorySelect = document.getElementById("subcategory-select");
 const rollButton = document.getElementById("roll-button");
@@ -82,10 +84,6 @@ const roll = (sides) => {
   return Math.floor(Math.random() * sides);
 };
 
-const roll2d6 = () => {
-  return roll(6) + roll(6);
-};
-
 // Output formatting functions
 const formatObjectToString = (obj) => {
   return Object.entries(obj)
@@ -152,7 +150,8 @@ const rollBestiary = (data, subcategory) => {
     const textResult = `<b><u>Custom Monster</u></b><br><br><b>Physique:</b> ${physique}<br><b>Feature:</b> ${feature}<br><b>Quirks:</b> ${quirks}<br><b>Weakness:</b> ${weakness}<br><b>Attack:</b> ${attack}<br><b>Critical Damage:</b> ${criticalDamage}<br><b>Ability:</b> ${ability}<br><b>Target:</b> ${target}`;
     displayResult(textResult);
   } else if (subcategory === "Reaction Roll") {
-    const result = bestiary[roll2d6()];
+    let roll = utils.roll(2, 6);
+    const result = bestiary[roll - 2]; //2-12 -> 0-10
     const textResult = `<b><u>Reaction Roll</u></b><br><br><b>${result}</b>`;
     displayResult(textResult);
   }
