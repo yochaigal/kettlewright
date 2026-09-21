dd727ea8f419fb8773d5bd9b004f08f41f245d68
Yochai Gal <yochaigal@gmail.com>
2026-09-21 14:22:14 -0400

    Update entrypoint.sh to use gevent

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ WORKERS=${WORKERS:-1}
 # Check if Redis should be used
 if [ "$USE_REDIS" = "True" ] || [ "$USE_REDIS" = "true" ]; then
     # Start the application with Redis and the specified number of workers
-    exec gunicorn -k eventlet -w $WORKERS -b 0.0.0.0:8000 --timeout 120 app:application
+    exec gunicorn -k gevent -w $WORKERS -b 0.0.0.0:8000 --timeout 120 app:application
 else
     # Start the application without Redis and the specified number of workers
-    exec gunicorn -k eventlet -w $WORKERS -b 0.0.0.0:8000 --timeout 120 app:application
+    exec gunicorn -k gevent -w $WORKERS -b 0.0.0.0:8000 --timeout 120 app:application
 fi
