282d8fb489e982547098f2854e8f731ab593d855
Danil Rodin <danre1@bk.ru>
2026-09-24 11:27:44 +0300

    fix for portrait upload on mobile #300

--- app/static/src/js/party_view.js
+++ app/static/src/js/party_view.js
@@ -2,8 +2,37 @@ import utils from "./utils.js";
 
 window.KW_alert = utils.styledAlert;
 
+function updateWildernessAction(form) {
+  const hasPC = Boolean(form.querySelector('input[name="participants"]:checked'));
+  const wrapper = form.querySelector('.wilderness-submit');
+  wrapper.querySelector('button').disabled = !hasPC;
+  if (hasPC) {
+    wrapper.removeAttribute('title');
+    wrapper.removeAttribute('aria-label');
+    wrapper.removeAttribute('tabindex');
+  } else {
+    wrapper.title = wrapper.dataset.selectionHint;
+    wrapper.setAttribute('aria-label', wrapper.dataset.selectionHint);
+    wrapper.tabIndex = 0;
+  }
+  return hasPC;
+}
+
+// Browsers may restore checkbox values when returning through history.
+window.addEventListener('pageshow', () => {
+  document.querySelectorAll('.wilderness-form').forEach(updateWildernessAction);
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOMContentLoaded");
+
+  document.querySelectorAll('.wilderness-form').forEach((form) => {
+    updateWildernessAction(form);
+    form.addEventListener('change', () => updateWildernessAction(form));
+    form.addEventListener('submit', (event) => {
+      if (!updateWildernessAction(form)) event.preventDefault();
+    });
+  });
 
   // prepare join code button
   if (document.getElementById("join-code-button")) {
